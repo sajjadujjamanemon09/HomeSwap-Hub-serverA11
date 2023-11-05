ed780fff08073c514f33249d5ab7e1216298fe8b
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-11-05 17:11:42 +0600

    adding services & booking collection in MongoDb

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(express.json());
 
 
 
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hepooac.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,10 +28,34 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
-    
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+    // connect collection
+    const serviceCollection = client.db('homeSwapDB').collection('services')
+    const bookingCollection = client.db('homeSwapDB').collection('bookings')
+
+    app.get('/api/v1/services', async (rew, res) => {
+      const cursor = serviceCollection.find()
+      const result = await cursor.toArray()
+    
+      res.send(result)
+    })
+    app.post('/api/v1/user/create-booking', (req, res) => {
+      req.body;
+      // res.send()
+    })
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -42,6 +69,9 @@ run().catch(console.dir);
 
 
 
+
+
+
 // server connection for all project
 app.get('/', (req, res) => {
     res.send('setUp server running')
@@ -49,6 +79,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`setUp project server running on PORT: ${port}`);
 })
-
-
-
