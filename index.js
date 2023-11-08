9c3acee13026c392d6f7d938c716ab587b3fcf14
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-11-08 20:13:31 +0600

    await client close

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const verifyToken = (req, res, next) =>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // connect collection
     const serviceCollection = client.db("homeSwapDB").collection("services");
